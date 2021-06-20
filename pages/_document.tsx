5326f801d8cc11da7d44f9e79f9babfda66237eb
Luke Bennett <hello@lukebennett.com.au>
2021-06-20 21:27:06 +1000

    Add custom Document

---
+++ pages/_document.tsx
@@ -0,0 +1,27 @@
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
+
+class AppDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+  render() {
+    return (
+      <Html lang="en-AU">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default AppDocument;
